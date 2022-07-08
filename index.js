cf533abdbb9b5ea7de15e49f42b136d9be0bf55d
Rzkkokok <464042964@qq.com>
2022-07-08 19:58:55 +0800

    add list point

--- index.js
+++ index.js
@@ -3,25 +3,37 @@ const http = require(`http`);
 const url = require(`url`);
 const axios = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
+const express = request(`express`);
 
+const app = express();
 const token = `5495241708:AAH3HaZ8LcgGFB_rfG9FwuqHG60LddBg6SU`;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
+// start
 bot.onText(/\/start/,msg=>{
     bot.sendMessage(msg.chat.id,'Welcome my friend');
 });
 
+const prot = process.env.prot;
+
+// check game num
 bot.onText(/\/game/,msg=>{
         var result = axios.get(`https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1?appid=1509510`).then(
         (result) =>{
             //console.log(result.data.response.player_count);
             bot.sendMessage(msg.chat.id,'current game player: ' + result.data.response.player_count);
-
+            
         }
     );
 });
 
+
+app.get(`/`,(req,res) =>{
+    res.send(`telegram  bot`);
+})
+
+app.listen(prot);
 //const gameNum = 1509510;
 //var result = axios.get(`https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1?appid=${gameNum}`).then(
 
